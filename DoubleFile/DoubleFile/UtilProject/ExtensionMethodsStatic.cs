98b4aece16257c35625687513ca1e3ec956ba99b
Tim Richards <tim.richards.dev@gmail.com>
2015-02-11 14:26:44 -0500

    Revert "Enumerator vs Count test: Count wins (revert)"
    This reverts commit afdb54e62e7b69318381e37c384a67c8999f4b3b.

--- DoubleFile/DoubleFile/UtilProject/ExtensionMethodsStatic.cs
+++ DoubleFile/DoubleFile/UtilProject/ExtensionMethodsStatic.cs
@@ -12,77 +12,29 @@ namespace DoubleFile
             return (false == source.GetEnumerator().MoveNext());
         }
 
-        internal struct dt
-        {
-            internal TimeSpan Count;
-            internal TimeSpan Enumerator;
-        }
-
-        static internal dt[] atsDT = new dt[6];
-
         internal static bool IsEmpty<T>(this UList<T> source)
         {
-            DateTime dtCount = DateTime.Now;
-            bool bRetA = (source.Count <= 0);
-            atsDT[0].Count += DateTime.Now - dtCount;
-
-            DateTime dtEnumerator = DateTime.Now;
-            bool bRetB = (false == source.GetEnumerator().MoveNext());
-            atsDT[0].Enumerator += DateTime.Now - dtEnumerator;
-
-            return bRetA;
+            return (source.Count <= 0);
         }
 
         internal static bool IsEmpty(this ListView.ListViewItemCollection source)
         {
-            DateTime dtCount = DateTime.Now;
-            bool bRetA = (source.Count <= 0);
-            atsDT[1].Count += DateTime.Now - dtCount;
-
-            DateTime dtEnumerator = DateTime.Now;
-            bool bRetB = (false == source.GetEnumerator().MoveNext());
-            atsDT[1].Enumerator += DateTime.Now - dtEnumerator;
-
-            return bRetA;
+            return (source.Count <= 0);
         }
 
         internal static bool IsEmpty(this ListView.SelectedListViewItemCollection source)
         {
-            DateTime dtCount = DateTime.Now;
-            bool bRetA = (source.Count <= 0);
-            atsDT[2].Count += DateTime.Now - dtCount;
-
-            DateTime dtEnumerator = DateTime.Now;
-            bool bRetB = (false == source.GetEnumerator().MoveNext());
-            atsDT[2].Enumerator += DateTime.Now - dtEnumerator;
-
-            return bRetA;
+            return (source.Count <= 0);
         }
 
         internal static bool IsEmpty(this TreeNodeCollection source)
         {
-            DateTime dtCount = DateTime.Now;
-            bool bRetA = (source.Count <= 0);
-            atsDT[3].Count += DateTime.Now - dtCount;
-
-            DateTime dtEnumerator = DateTime.Now;
-            bool bRetB = (false == source.GetEnumerator().MoveNext());
-            atsDT[3].Enumerator += DateTime.Now - dtEnumerator;
-
-            return bRetA;
+            return (source.Count <= 0);
         }
 
         internal static bool IsEmptyA(this System.Collections.IList source)
         {
-            DateTime dtCount = DateTime.Now;
-            bool bRetA = (source.Count <= 0);
-            atsDT[4].Count += DateTime.Now - dtCount;
-
-            DateTime dtEnumerator = DateTime.Now;
-            bool bRetB = (false == source.GetEnumerator().MoveNext());
-            atsDT[4].Enumerator += DateTime.Now - dtEnumerator;
-
-            return bRetA;
+            return (source.Count <= 0);
         }
 
         internal static IEnumerator<T> First<T>(this IEnumerable<T> source, Action<T> action)
@@ -110,21 +62,12 @@ namespace DoubleFile
 
         internal static bool HasOnlyOne(this System.Collections.IList source)
         {
-            DateTime dtCount = DateTime.Now;
-            var bRetA = (source.Count == 1);
-            atsDT[5].Count += DateTime.Now - dtCount;
-
-            DateTime dtEnumerator = DateTime.Now;
             var enumerator = source.GetEnumerator();
 
-            var bRetB = false;
+            if (false == enumerator.MoveNext())
+                return false;
 
-            if (enumerator.MoveNext())
-                bRetB = (false == enumerator.MoveNext());
-
-            atsDT[5].Enumerator += DateTime.Now - dtEnumerator;
-
-            return bRetA;
+            return (false == enumerator.MoveNext());
         }
     }
 

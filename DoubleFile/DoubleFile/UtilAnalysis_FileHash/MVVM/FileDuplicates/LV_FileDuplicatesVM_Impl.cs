8fc175d7b04af2af0a132a242d62c85a9a2dd25a
Tim Richards <tim.richards.dev@gmail.com>
2015-02-28 21:29:38 -0500

    1.0.0.66 yeah this is simpler and it works. There's no speedup with all the enumberable stuff

--- DoubleFile/DoubleFile/UtilAnalysis_FileHash/MVVM/FileDuplicates/LV_FileDuplicatesVM_Impl.cs
+++ DoubleFile/DoubleFile/UtilAnalysis_FileHash/MVVM/FileDuplicates/LV_FileDuplicatesVM_Impl.cs
@@ -37,72 +37,40 @@ namespace DoubleFile
                 List<int> lsLineNumbers = new List<int>();
 
                 foreach (var duplicate in g)
-                    lsLineNumbers.Add(duplicate.LineNumber);
-
-                MBoxStatic.Assert(99903, 0 < lsLineNumbers.Count);
-                lsLineNumbers.Sort();               // jic already sorted upstream at A
-
-                var ieMain =
-                    File
-                    .ReadLines(g.Key)
-                    .Skip(lsLineNumbers[0] - 1);
-                List<string> lsFilesInDir = new List<string>();
-
-                while (true)
                 {
-                    lsFilesInDir.Add(
-                        ieMain
-                        .Take(1)
-                        .ToArray()[0]);
-
-                    var nLineNumber = lsLineNumbers[0];
-                    var nNextFileLineNumber = -1;
-
-                    lsLineNumbers.RemoveAt(0);
-
-                    if (0 < lsLineNumbers.Count)
-                        nNextFileLineNumber = lsLineNumbers[0];
-
-                    ieMain =
-                        ieMain
-                        .SkipWhile(strLine =>
-                            (false == strLine.StartsWith(FileParse.ksLineType_Directory) &&
-                            (nLineNumber++ < nNextFileLineNumber - 1)));
-
-                    var bBreak = false;
-
-                    ieMain
-                        .First(strLine =>
-                    {
-                        if (strLine.StartsWith(FileParse.ksLineType_Directory))
-                        {
-#if (DEBUG)
-                            MBoxStatic.Assert(99905, strLine.Split('\t')[1] == "" + nLineNumber);
-#endif
-                            foreach (var strLineOut in lsFilesInDir)
-                                lsLines.Add(new[] { strLineOut.Split('\t')[3], strLine.Split('\t')[2] });
-
-                            lsFilesInDir.Clear();
-
-                            if (0 < lsLineNumbers.Count)
-                            {
-                                ieMain =
-                                    ieMain
-                                    .Skip(nNextFileLineNumber - nLineNumber);
-
-                                nLineNumber = nNextFileLineNumber;
-                            }
-                        }
-
-                        if (0 == lsLineNumbers.Count)
-                            bBreak = true;
-                    });
-
-                    if (bBreak)
-                        break;
+                    lsLineNumbers.Add(duplicate.LineNumber);
                 }
 
-                MBoxStatic.Assert(99904, 0 == lsLineNumbers.Count);
+                lsLineNumbers.Sort();               // jic already sorted upstream at A
+
+                int nLine = 0;
+                List<string> lsFilesInDir = new List<string>();
+                var nMatchLine = lsLineNumbers[0];
+
+                foreach (var strLine
+                    in File.ReadLines(g.Key))
+                {
+                    ++nLine;
+
+                    if (nLine == nMatchLine)
+                    {
+                        lsFilesInDir.Add(strLine);
+                        lsLineNumbers.RemoveAt(0);
+                        nMatchLine = (lsLineNumbers.Count > 0) ? lsLineNumbers[0] : -1;
+                    }
+                    else if (strLine.StartsWith(FileParse.ksLineType_Directory))
+                    {
+                        foreach (var strFileLine in lsFilesInDir)
+                            lsLines.Add(new[] { strFileLine.Split('\t')[3], strLine.Split('\t')[2] });
+
+                        lsFilesInDir.Clear();
+ 
+                        if (0 == lsLineNumbers.Count)
+                            break;
+                   }
+                }
+
+                MBoxStatic.Assert(99908, 0 == lsLineNumbers.Count);
             });
 
             UtilProject.UIthread(() => 

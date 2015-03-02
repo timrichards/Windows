7c3b831e7490c3fd68a4c86c6df486544db37e6b
Tim Richards <tim.richards.dev@gmail.com>
2015-03-01 19:08:01 -0500

    optimize barely if any

--- DoubleFile/DoubleFile/UtilAnalysis_FileHash/MVVM/FileDuplicates/LV_FileDuplicatesVM_Impl.cs
+++ DoubleFile/DoubleFile/UtilAnalysis_FileHash/MVVM/FileDuplicates/LV_FileDuplicatesVM_Impl.cs
@@ -40,12 +40,14 @@ namespace DoubleFile
 
                 lsLineNumbers.Sort();               // jic already sorted upstream at A
 
-                var nLine = 0;
                 var lsFilesInDir = new List<string>();
                 var nMatchLine = lsLineNumbers[0];
+                var nLine = nMatchLine - 1;
 
                 foreach (var strLine
-                    in File.ReadLines(g.Key))
+                    in File
+                    .ReadLines(g.Key)
+                    .Skip(nLine))
                 {
                     ++nLine;
 

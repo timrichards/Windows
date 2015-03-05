324a688d13bfd26464a3171b21b1f1066d070c83
Tim Richards <tim.richards.dev@gmail.com>
2015-03-05 13:32:25 -0500

    hack experiment slowed it down

--- DoubleFile/DoubleFile/UtilAnalysis_FileHash/MVVM_WinFileHash_Folders/TreeViewItem_FileHashVM_Impl.cs
+++ DoubleFile/DoubleFile/UtilAnalysis_FileHash/MVVM_WinFileHash_Folders/TreeViewItem_FileHashVM_Impl.cs
@@ -8,11 +8,6 @@ namespace DoubleFile
     {
         void DoTreeSelect()
         {
-            if (_bSelectProgrammatic)
-            {
-                return;
-            }
-
             if (false == _bSelected)
             {
                 return;
@@ -26,10 +21,7 @@ namespace DoubleFile
             if ((0 == asPath.Count()) ||
                 (1 == asPath.Count()) && string.IsNullOrWhiteSpace(asPath.ElementAt(0)))
             {
-                _bSelectProgrammatic = true;
                 SelectProgrammatic(true);
-                new Local.TreeSelect(_datum, _TVVM._dictVolumeInfo, false, false).DoThreadFactory();
-                _bSelectProgrammatic = false;
                 return;
             }
 
@@ -43,7 +35,5 @@ namespace DoubleFile
                         item.GoToFile(asPath.Skip(1), strFile);
                     }));
         }
-
-        bool _bSelectProgrammatic = false;
     }
 }

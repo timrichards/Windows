47c73bb546dd3d7381bec01a90980daea0747d5e
Tim Richards <tim.richards.dev@gmail.com>
2015-04-03 08:19:43 -0400

    ScreenOffWhenLocked

--- ScreenOffWhenLocked/ScreenOffWhenLocked/Program.cs
+++ ScreenOffWhenLocked/ScreenOffWhenLocked/Program.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.Win32;
 using System;
-using System.IO;
 using System.Runtime.InteropServices;
-using System.Threading;
 using System.Windows.Forms;
 using System.Drawing;
 
@@ -13,21 +11,38 @@ namespace ScreenOffWhenLocked
         [STAThread]
         static void Main()
         {
-            _form = new Form();
-            _form.Load += (o, e) => SystemEvents.SessionSwitch += SystemEvents_SessionSwitch;
-            _form.Location = new Point(10000, 10000);
-            _form.StartPosition = FormStartPosition.Manual;
-            _form.ShowInTaskbar = false;
-            _form.ShowDialog();
+            new Form1().ShowDialog();
+        }
+    }
+
+    class Form1 : Form
+    {
+        internal Form1()
+        {
+            _timer = new System.Windows.Forms.Timer() { Interval = 60000 };
+            _timer.Tick += (o, e) => SendMessage();
+
+            Location = new Point(10000, 10000);
+            Size = new Size(0, 0);
+            FormBorderStyle = FormBorderStyle.None;
+            ShowIcon = false;
+            Opacity = 0;
+            Icon = null;
+            StartPosition = FormStartPosition.Manual;
+            ShowInTaskbar = false;
+
+            Load += (o, e) =>
+            {
+                Hide();
+                Enabled = false;
+            };
+
+            SystemEvents.SessionSwitch += SystemEvents_SessionSwitch;
         }
 
         [DllImport("user32.dll", CharSet = CharSet.Auto)]
         static extern IntPtr
             SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, IntPtr lParam);
-
-        [DllImport("user32.dll", SetLastError = false)]
-        static extern IntPtr
-            GetDesktopWindow();
 
         const Int32
             WM_SYSCOMMAND = 0x112;
@@ -35,20 +50,38 @@ namespace ScreenOffWhenLocked
         const UInt32
             SC_MONITORPOWER = 0xF170;
 
-        static void SystemEvents_SessionSwitch(object sender, Microsoft.Win32.SessionSwitchEventArgs e)
+        void SendMessage()
         {
-            if (e.Reason != SessionSwitchReason.SessionLock)
-                return;
-
             //  1       Low power
             //  2       Off
             // -1       On
-            if (null == _form)
-                _form = new Form();
 
-            SendMessage(_form.Handle, WM_SYSCOMMAND, (IntPtr)SC_MONITORPOWER, (IntPtr)2);
+            SendMessage(Handle, WM_SYSCOMMAND, (IntPtr)SC_MONITORPOWER, (IntPtr)2);
         }
 
-        static Form _form = null;
+        void SystemEvents_SessionSwitch(object sender, Microsoft.Win32.SessionSwitchEventArgs e)
+        {
+            switch (e.Reason)
+            {
+                case SessionSwitchReason.SessionLock:
+                {
+                    SendMessage();
+                    _timer.Start();
+                    break;
+                }
+
+                case SessionSwitchReason.SessionUnlock:
+                {
+                    _timer.Stop();
+                    break;
+                }
+
+                default:
+                    return;
+            }
+        }
+
+        System.Windows.Forms.Timer
+            _timer = null;
     }
 }

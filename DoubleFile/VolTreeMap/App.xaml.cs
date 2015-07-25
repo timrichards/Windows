40aca0795ff9c1b75592c7747856e260c73ee96b
Tim Richards <tim.richards.dev@gmail.com>
2015-07-24 22:35:30 -0400

    VolTreeMap project

---
+++ DoubleFile/VolTreeMap/App.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Windows;
+using DoubleFile;
+using System.Reactive.Linq;
+
+namespace VolTreeMap
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        static internal IObservable<Tuple<bool, int>>   // bool is a no-op: generic placeholder
+            DeactivateDidOccur => _deactivateDidOccur;
+        static readonly LocalSubject<bool> _deactivateDidOccur = new LocalSubject<bool>();
+        static void DeactivateDidOccurOnNext() { _deactivateDidOccur.LocalOnNext(false, 99839); }
+
+        static internal bool CanFlashWindow_ResetsIt
+        {
+            get
+            {
+                if (_canFlashWindow_ResetsIt)
+                    return true;
+
+                _canFlashWindow_ResetsIt = true;
+                return false;
+            }
+        }
+        static bool _canFlashWindow_ResetsIt = true;
+
+        public App()
+        {
+            Statics.AppActivated = true;      // Application_Activated() seemed to work but jic
+
+            Observable.FromEventPattern(this, "Activated")
+                .Subscribe(x => Application_Activated());
+
+            Observable.FromEventPattern(this, "Deactivated")
+                .Subscribe(x => { Statics.AppActivated = false; DeactivateDidOccurOnNext(); });
+
+#if (false == DEBUG)
+            Observable.FromEventPattern<System.Windows.Threading.DispatcherUnhandledExceptionEventArgs>(this, "DispatcherUnhandledException")
+                .Subscribe(args =>
+            {
+                args.EventArgs.Handled = true;
+                MBoxStatic.Assert(-1, false, args.EventArgs.Exception.Message);
+            });
+#endif
+            ShutdownMode = ShutdownMode.OnMainWindowClose;
+        }
+
+        static void Application_Activated()
+        {
+            if (false == Statics.AppActivated)
+                _canFlashWindow_ResetsIt = false;
+            
+            Statics.AppActivated = true;
+        }
+
+        Statics
+            _statics = new Statics();
+    }
+}
---
+++ DoubleFile/VolTreeMap/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace VolTreeMap
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        static Action Init = null;
+        static void InitForMainWindowOnly(Action init) { Init = init; }
+        public
+            MainWindow()
+            : base(InitForMainWindowOnly)
+        {
+     //       Statics.Icon = Icon;
+            Init();
+            Init = null;
+            InitializeComponent();
+        }
+    }
+}

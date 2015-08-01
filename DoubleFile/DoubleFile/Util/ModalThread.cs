d914eca6c1ae63d58c3e7d99fbc6316cb8cd153a
Tim Richards <tim.richards.dev@gmail.com>
2015-07-31 22:07:27 -0400

    exhibit A does in the other

--- DoubleFile/DoubleFile/Util/ModalThread.cs
+++ DoubleFile/DoubleFile/Util/ModalThread.cs
@@ -5,9 +5,6 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Linq;
-using System.Windows.Interop;
-using Drawing = System.Drawing;
-using System.Windows.Media.Imaging;
 using System.Threading;
 
 namespace DoubleFile

4b2a87cdbcf6e0026e5fea8390fb2b491c49cb22
Tim Richards <tim.richards.dev@gmail.com>
2015-09-29 05:21:28 -0400

    VolTreeMap assembly: don't show color code descriptions

--- DoubleFile/DoubleFile/UserControls/MVVM_Detail/LV_FolderDetailVM_Impl.cs
+++ DoubleFile/DoubleFile/UserControls/MVVM_Detail/LV_FolderDetailVM_Impl.cs
@@ -33,26 +33,30 @@ namespace DoubleFile
                     folderDetail.ieDetail
                     .Select(ieLine => new LVitem_FolderDetailVM(ieLine.ToList()));
 
-                var strFG_Description = UtilColorcode.Descriptions[folderDetail.treeNode.ForeColor];
-                var strBG_Description = UtilColorcode.Descriptions[folderDetail.treeNode.BackColor];
-
-                if (false == string.IsNullOrWhiteSpace(strFG_Description))
+                if (Statics.Namespace == GetType().Namespace)     // VolTreeMap assembly: skip it
                 {
-                    ieDetail = ieDetail.Concat(new[]
-                    { new LVitem_FolderDetailVM(new[] { "", strFG_Description }) { Foreground = folderDetail.treeNode.Foreground } });
-                }
+                    var strFG_Description = UtilColorcode.Descriptions[folderDetail.treeNode.ForeColor];
+                    var strBG_Description = UtilColorcode.Descriptions[folderDetail.treeNode.BackColor];
 
-                if (false == string.IsNullOrWhiteSpace(strBG_Description))
-                {
-                    ieDetail = ieDetail.Concat(new[]
-                    { new LVitem_FolderDetailVM(new[] { "", strBG_Description }) { Background = folderDetail.treeNode.Background } });
-                }
+                    if (false == string.IsNullOrWhiteSpace(strFG_Description))
+                    {
+                        ieDetail = ieDetail.Concat(new[]
+                        { new LVitem_FolderDetailVM(new[] { "", strFG_Description }) { Foreground = folderDetail.treeNode.Foreground } });
+                    }
+
+                    if (false == string.IsNullOrWhiteSpace(strBG_Description))
+                    {
+                        ieDetail = ieDetail.Concat(new[]
+                        { new LVitem_FolderDetailVM(new[] { "", strBG_Description }) { Background = folderDetail.treeNode.Background } });
+                    }
 #if DEBUG
-                var nHashVersion = (Statics.DupeFileDictionary.AllListingsHashV2) ? "1 MB" : "4K";
+                    var nHashVersion = (Statics.DupeFileDictionary.AllListingsHashV2) ? "1 MB" : "4K";
 
-                ieDetail = ieDetail.Concat(new[]
-                { new LVitem_FolderDetailVM(new[] { nHashVersion + " All files hash", "" + folderDetail.treeNode.NodeDatum.Hash_AllFiles}) });
+                    ieDetail = ieDetail.Concat(new[]
+                    { new LVitem_FolderDetailVM(new[] { nHashVersion + " All files hash", "" + folderDetail.treeNode.NodeDatum.Hash_AllFiles}) });
 #endif
+                }
+
                 Title = folderDetail.treeNode.PathShort;
                 Add(ieDetail);
             });
